82fb94214d547f61996684ca703ccca90de79199
Chris <chris.goringe@gmail.com>
2023-11-27 10:50:40 +1100

    improved handling of new images

--- aesthetic_score_from_model.py
+++ aesthetic_score_from_model.py
@@ -5,10 +5,10 @@ from src.ap.database import Database
 from src.ap.clip import CLIP
 from src.time_context import Timer
 
-import os, statistics, math
+import os, statistics
 
 def main():
-    get_args(aesthetic_ab=True, aesthetic_training=True)
+    get_args(aesthetic_model=True)
     assert args['top_level_image_directory'], "Need an image directory"
     assert args['load_model'], "Need to load a model"
 
@@ -18,40 +18,36 @@ def main():
         clipper = CLIP(image_directory=args['top_level_image_directory'])
         predictor = AestheticPredictor(pretrained=os.path.join(args['load_model'],"model.safetensors"), 
                                     relu=args.get('aesthetic_model_relu',True), clipper=clipper)
-    
-    with Timer("Analyse existing non-zero scores") as logger:
-        all_non_zero_scores = [db.image_scores[f] for f in db.image_scores if db.image_scores[f]!=0]
-        if len(all_non_zero_scores)>1:
-            std_existing = statistics.stdev(all_non_zero_scores)
-            logger(f"Std dev. of {len(all_non_zero_scores)} existing scores = {std_existing}")
-            logger(f"(mean of existing scores = {statistics.mean(all_non_zero_scores)})")
-        else:
-            logger("No images have been scored: setting std dev. to 0.5")
-            std_existing = 0.5
-
+        
     with Timer("Predict scores for all images"):
         all_predicted_scores = predictor.evaluate_files([os.path.join(args['top_level_image_directory'],f) for f in db.image_scores], 
                                             as_sorted_tuple=True, eval_mode=True)
-        clipper.save_cache()
-        
         all_predicted_scores = [(float(a[0]), os.path.relpath(a[1],args['top_level_image_directory'])) for a in all_predicted_scores ]
-        mean = statistics.mean([a[0] for a in all_predicted_scores])
+        clipper.save_cache()
     
-    with Timer("Offset by mean and apply tanh") as logger:
-        new_predictions = { a[1]: math.tanh(a[0]-mean) for a in all_predicted_scores if db.image_scores[a[1]]==0 }
-        if len(new_predictions)<2:
-            logger(f"Can't add fewer than two new images - got {len(new_predictions)}")
-            return
-        std_predictions = statistics.stdev([new_predictions[a] for a in new_predictions])
-        mean_predictions = statistics.mean([new_predictions[a] for a in new_predictions])
-        logger(f"Std dev. of prediction scores = {std_predictions}")
-        logger(f"(mean of prediction scores = {mean_predictions})")
+    with Timer("Analyse statistics") as logger:
+        new_predictions = { a[1]: a[0] for a in all_predicted_scores if db.image_scores[a[1]]==0 }
+        old_predictions = { a[1]: a[0] for a in all_predicted_scores if db.image_scores[a[1]]!=0 }
+        assert len(new_predictions)>1 and len(old_predictions)>1 , "Need at least two old images and two new images"
+        mean_old_predictions = statistics.mean(old_predictions[a] for a in old_predictions)
+        std_old_predictions = statistics.stdev(old_predictions[a] for a in old_predictions)
+
+        old_db_scores = [db.image_scores[f] for f in db.image_scores if db.image_scores[f]!=0]
+        std_db = statistics.stdev(old_db_scores)
+        mean_db = statistics.mean(old_db_scores)
+
+        prediction_to_db = lambda a : (std_db*(a - mean_old_predictions)/std_old_predictions) + mean_db
+    
+        logger("For existing images, model score mean {:>6.3f} stdev {:>6.3f}, database score mean {:>6.3f} stdev {:>6.3f}".format(
+            mean_old_predictions, std_old_predictions, mean_db, std_db))
+        
+        logger("Scaling of model score a to database score is therefore lambda a : ({:>6.3f} * (a -{:>6.3f}) /{:>6.3f}) +{:>6.3f}".format(
+            std_db, mean_old_predictions, std_old_predictions, mean_db ))
 
     with Timer("Set scores for unscored images") as logger:
-        scaling = std_existing/std_predictions
-        logger(f"scaling new predictions by {scaling}")
+
         for f in new_predictions:
-            db.image_scores[f] = new_predictions[f]*scaling
+            db.image_scores[f] = prediction_to_db(new_predictions[f])
         logger(f"Added scores for {len(new_predictions)} unscored images")
 
     with Timer("Save updated database"):
