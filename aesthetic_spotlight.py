ff6b1c773c7f8e8f91d14246fc92a8949b1f4009
Chris <chris.goringe@gmail.com>
2023-12-06 23:40:29 +1100

    get clip

--- aesthetic_spotlight.py
+++ aesthetic_spotlight.py
@@ -12,18 +12,19 @@ def main():
     top_level_images = args['top_level_image_directory']
     
     with Timer('load models'):
-        clipper = CLIP(pretrained=args['clip_model'], image_directory=top_level_images)
+        clipper = CLIP.get_clip(pretrained=args['clip_model'], image_directory=top_level_images)
         predictor = AestheticPredictor(pretrained=args['load_model_path'], clipper=clipper)
 
     with Timer("prepare data"):
         df = DataFrame()#columns=['image','db_score','model_score','db_rank','model_rank'])
         database_scores = ImageScores.from_scorefile(top_level_images)
-        model_scores = ImageScores.from_evaluator(predictor.evaluate_file, database_scores.image_files(), database_scores.image_directory)
+        model_scores = ImageScores.from_evaluator(predictor.evaluate_file, database_scores.image_files(), top_level_images)
         df['image'] = database_scores.image_files(fullpath=True)
         df['db_score'] = database_scores.scores()
         df['model_score'] = model_scores.scores()
         df['db_rank'] = database_scores.ranks()
         df['model_rank'] = model_scores.ranks()
+        clipper.save_cache()
 
     spotlight.show(df)
 
