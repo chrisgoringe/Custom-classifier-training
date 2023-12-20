8b164576b76bbf25037cfb6aa1d37583bd4eb318
Chris <chris.goringe@gmail.com>
2023-12-20 16:38:40 +1100

    l and g

--- create_aesthetic_nudge.py
+++ create_aesthetic_nudge.py
@@ -3,6 +3,7 @@ from src.ap.clip import CLIP
 from src.ap.image_scores import ImageScores
 import torch, os
 from safetensors.torch import save_file
+from tqdm import tqdm
 
 '''
 For the given image directory (which is assumed to have been AB scored), create a nudge file.
@@ -13,9 +14,9 @@ I think you probably have to use openai/clip-vit-large-patch14 because that's th
 
 args = {
     'top_level_image_directory' : r"C:\Users\chris\Documents\GitHub\ComfyUI_windows_portable\ComfyUI\output",
-    'clip_model'                : "openai/clip-vit-large-patch14",   # DONT CHANGE
+    'clip_model'                : ["openai/clip-vit-large-patch14", "laion/CLIP-ViT-bigG-14-laion2B-39B-b160k"], 
     'load_model_path'           : None,
-    "input_size"                : 768,
+    "input_size"                : 2048,
 }
 def make_weighter(scores_dict):
     p = [ scores_dict[s] for s in scores_dict if scores_dict[s]>0 ]
@@ -29,17 +30,19 @@ def make_weighter(scores_dict):
 def make_nudge():
     dir = args['top_level_image_directory']
     scores:ImageScores = ImageScores.from_scorefile(dir)
-    clipper=CLIP.get_clip(image_directory=dir, pretrained=args['clip_model'])
     if args['load_model_path']:
         ap = AestheticPredictor(clipper=clipper, pretrained=args['load_model_path'])
         scores = ImageScores.from_evaluator(ap.evaluate_file, scores.image_files(), dir)
     scores_dict = scores.scores_dictionary(normalised=True)
     weighter = make_weighter(scores_dict)
-    nudge = torch.zeros((args['input_size'])).to('cuda')
-    for f in scores_dict:
-        features:torch.Tensor = clipper.prepare_from_file(os.path.join(args['top_level_image_directory'],f))
-        #print(torch.dot(features,features))
+
+    clipper = CLIP.get_clip(image_directory=dir, pretrained=args[f"clip_model"])
+    nudge = torch.zeros(args[f"input_size"]).cuda()
+    for f in tqdm(scores_dict):
+        features = clipper.prepare_from_file(os.path.join(args['top_level_image_directory'],f))
         nudge = nudge + features * weighter(scores_dict[f])
+
+    clipper.save_cache()
     save_file({'nudge':nudge}, os.path.join(args['top_level_image_directory'], 'nudge.safetensors'))
 
 if __name__=='__main__':
