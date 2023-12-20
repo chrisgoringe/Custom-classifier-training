ddd12f3b3d512a7b7916bc38ddea1ced097cc883
Chris <chris.goringe@gmail.com>
2023-12-20 16:38:19 +1100

    change clip loading

--- src/ap/clip.py
+++ src/ap/clip.py
@@ -2,38 +2,39 @@ import torch, clip
 from PIL import Image
 from safetensors.torch import save_file, load_file
 import os, sys
+from torch._tensor import Tensor
 from transformers import CLIPVisionModel, AutoProcessor, CLIPModel
 
 class CLIP:
     last_clip = None
     last_file = None
+    model:torch.nn.Module = None
 
     @classmethod
-    def get_clip(cls, pretrained="ViT-L/14", device="cuda", image_directory="."):
-        if cls.last_file and cls.last_file==pretrained: return cls.last_clip
-        try:
-            cls.last_clip = OpenAICLIP(pretrained, device, image_directory)
-        except:
-            print(f"Trying to load {pretrained} as OpenAICLIP model got error {sys.exc_info()[1]}")
-            print("Will try TransformersCLIP model...")
-        try:
-            cls.last_clip = TransformersCLIP(pretrained, device, image_directory)
-        except:
-            print(f"Trying to load {pretrained} as TransformersCLIP model got error {sys.exc_info()[1]}")
-            print("Giving up")
-            raise Exception(f"Failed to load {pretrained}")
-        cls.last_file = pretrained
+    def get_clip(cls, pretrained="ViT-L/14", device="cuda", image_directory=".", use_cache=True):
+        id = "_".join(pretrained) if isinstance(pretrained,list) else pretrained
+        if not (cls.last_file and cls.last_file==id):
+            if isinstance(pretrained,list):
+                cls.last_clip = MultiCLIP(pretrained, device, image_directory, use_cache)
+            elif pretrained in clip.available_models():
+                cls.last_clip = OpenAICLIP(pretrained, device, image_directory, use_cache)
+            else:
+                cls.last_clip = TransformersCLIP(pretrained, device, image_directory, use_cache)
+            cls.last_file = id
         return cls.last_clip
 
-    def setup(self, pretrained, image_directory):
-        self.cached = {}
-        self.cachefile = os.path.join(image_directory,f"clipcache.{pretrained.replace('/','_')}.safetensors")
+    def __init__(self, pretrained, device, image_directory, use_cache=True):
+        self.metadata = {"clip_model":pretrained}
+        self.device = device
         self.image_directory = image_directory
-        try:
-            self.cached = load_file(self.cachefile, device=self.device)
-            print(f"Reloaded CLIPs from {self.cachefile} - delete this file if you don't want to do that")
-        except:
-            print(f"Didn't reload CLIPs from {self.cachefile}")
+        if use_cache:
+            self.cached = {}
+            self.cachefile = os.path.join(image_directory,f"clipcache.{pretrained.replace('/','_')}.safetensors")
+            try:
+                self.cached = load_file(self.cachefile, device=self.device)
+                print(f"Reloaded CLIPs from {self.cachefile} - delete this file if you don't want to do that")
+            except:
+                print(f"Didn't reload CLIPs from {self.cachefile}")
 
     def prepare_from_file(self, filepath, device="cuda"):
         rel = os.path.relpath(filepath, self.image_directory)
@@ -49,32 +50,29 @@ class CLIP:
     
     def get_image_features_tensor(self, image:Image) -> torch.Tensor:
         raise NotImplementedError()
-
+    
+    def to(self, device:str):
+        self.model.to(device)
+        self.device = device
 
 class OpenAICLIP(CLIP):  
-    def __init__(self, pretrained="ViT-L/14", device="cuda", image_directory="."):
-        self.metadata = {"clip_model":pretrained}
+    def __init__(self, pretrained="ViT-L/14", device="cuda", image_directory=".", use_cache=True):
+        super().__init__(pretrained, device, image_directory, use_cache)
         self.model, self.preprocess = clip.load(pretrained, device=device, download_root="models/clip")
-        self.device = device
-        self.setup(pretrained, image_directory)
 
     def get_image_features_tensor(self, image:Image) -> torch.Tensor:
-        image = self.preprocess(image).unsqueeze(0).to(self.device)
         with torch.no_grad():
+            image = self.preprocess(image).unsqueeze(0).to(self.device)
             image_features = self.model.encode_image(image)
             image_features /= image_features.norm(dim=-1, keepdim=True)
             return image_features.to(torch.float)
    
 class TransformersCLIP(CLIP):
-    def __init__(self, pretrained="", device="cuda", image_directory="."):
-        self.metadata = {"clip_model":pretrained}
-        #self.model = CLIPVisionModel.from_pretrained(pretrained, cache_dir="models/clip",)
-        self.model = CLIPModel.from_pretrained(pretrained, cache_dir="models/clip",)
+    def __init__(self, pretrained="", device="cuda", image_directory=".", use_cache=True):
+        super().__init__(pretrained, device, image_directory, use_cache)
+        self.model = CLIPModel.from_pretrained(pretrained, cache_dir="models/clip")
         self.model.to(device)
         self.processor = AutoProcessor.from_pretrained(pretrained, cache_dir="models/clip")
-        self.device = device
-
-        self.setup(pretrained, image_directory)
 
     def get_image_features_tensor(self, image:Image) -> torch.Tensor:
         with torch.no_grad():
@@ -83,6 +81,20 @@ class TransformersCLIP(CLIP):
                 if isinstance(inputs[k],torch.Tensor): inputs[k] = inputs[k].to(self.device)
             outputs = self.model.get_image_features(output_hidden_states=True, **inputs)
             return outputs.flatten()
+        
+class MultiCLIP(CLIP):
+    def __init__(self, pretrained:list, device, image_directory, use_cache):
+        super().__init__("_".join(pretrained), device, image_directory, use_cache)
+        self.models = [ CLIP.get_clip(p, "cpu", image_directory, use_cache=False) for p in pretrained ]
+        
+    def get_image_features_tensor(self, image: Image) -> Tensor:
+        ift = None
+        for m in self.models:
+            m.to(self.device)
+            features = m.get_image_features_tensor(image)
+            ift = features if ift is None else torch.cat([ift,features])
+            m.model.to('cpu')
+        return ift
 
 if __name__=='__main__':
     c = CLIP.get_clip()
