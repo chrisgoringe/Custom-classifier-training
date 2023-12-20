1b4d2b773a0ebc2435dabc4451316b642bc7462f
Chris <chris.goringe@gmail.com>
2023-12-20 17:42:50 +1100

    bugfix

--- src/ap/clip.py
+++ src/ap/clip.py
@@ -4,6 +4,9 @@ from safetensors.torch import save_file, load_file
 import os, sys
 from torch._tensor import Tensor
 from transformers import CLIPVisionModel, AutoProcessor, CLIPModel
+from tqdm import tqdm
+
+OpenAIModels = ["RN50","RN101","RN50x4","RN50x16","RN50x64","ViT-B/32","ViT-B/16","ViT-L/14","ViT-L/14@336px"]
 
 class CLIP:
     last_clip = None
@@ -11,30 +14,30 @@ class CLIP:
     model:torch.nn.Module = None
 
     @classmethod
-    def get_clip(cls, pretrained="ViT-L/14", device="cuda", image_directory=".", use_cache=True):
+    def get_clip(cls, pretrained="ViT-L/14", device="cuda", image_directory="."):
         id = "_".join(pretrained) if isinstance(pretrained,list) else pretrained
         if not (cls.last_file and cls.last_file==id):
             if isinstance(pretrained,list):
-                cls.last_clip = MultiCLIP(pretrained, device, image_directory, use_cache)
-            elif pretrained in clip.available_models():
-                cls.last_clip = OpenAICLIP(pretrained, device, image_directory, use_cache)
+                cls.last_clip = MultiCLIP(pretrained, device, image_directory)
+            elif pretrained in OpenAIModels:
+                cls.last_clip = OpenAICLIP(pretrained, device, image_directory)
             else:
-                cls.last_clip = TransformersCLIP(pretrained, device, image_directory, use_cache)
+                cls.last_clip = TransformersCLIP(pretrained, device, image_directory)
             cls.last_file = id
         return cls.last_clip
 
-    def __init__(self, pretrained, device, image_directory, use_cache=True):
+    def __init__(self, pretrained, device, image_directory):
         self.metadata = {"clip_model":pretrained}
         self.device = device
         self.image_directory = image_directory
-        if use_cache:
-            self.cached = {}
-            self.cachefile = os.path.join(image_directory,f"clipcache.{pretrained.replace('/','_')}.safetensors")
-            try:
-                self.cached = load_file(self.cachefile, device=self.device)
-                print(f"Reloaded CLIPs from {self.cachefile} - delete this file if you don't want to do that")
-            except:
-                print(f"Didn't reload CLIPs from {self.cachefile}")
+
+        self.cached = {}
+        self.cachefile = os.path.join(image_directory,f"clipcache.{pretrained.replace('/','_')}.safetensors")
+        try:
+            self.cached = load_file(self.cachefile, device=self.device)
+            print(f"Reloaded CLIPs from {self.cachefile} - delete this file if you don't want to do that")
+        except:
+            print(f"Didn't reload CLIPs from {self.cachefile}")
 
     def prepare_from_file(self, filepath, device="cuda"):
         rel = os.path.relpath(filepath, self.image_directory)
@@ -42,6 +45,10 @@ class CLIP:
             self.cached[rel] = self.get_image_features_tensor(Image.open(filepath))
         return self.cached[rel].to(device)
     
+    def precache(self, filepaths):
+        for filepath in filepaths:
+            self.prepare_from_file(filepath)
+
     def save_cache(self):
         save_file(self.cached, self.cachefile)
 
@@ -55,9 +62,12 @@ class CLIP:
         self.model.to(device)
         self.device = device
 
+    def simplify(self):
+        pass
+
 class OpenAICLIP(CLIP):  
-    def __init__(self, pretrained="ViT-L/14", device="cuda", image_directory=".", use_cache=True):
-        super().__init__(pretrained, device, image_directory, use_cache)
+    def __init__(self, pretrained="ViT-L/14", device="cuda", image_directory="."):
+        super().__init__(pretrained, device, image_directory)
         self.model, self.preprocess = clip.load(pretrained, device=device, download_root="models/clip")
 
     def get_image_features_tensor(self, image:Image) -> torch.Tensor:
@@ -68,9 +78,10 @@ class OpenAICLIP(CLIP):
             return image_features.to(torch.float)
    
 class TransformersCLIP(CLIP):
-    def __init__(self, pretrained="", device="cuda", image_directory=".", use_cache=True):
-        super().__init__(pretrained, device, image_directory, use_cache)
+    def __init__(self, pretrained="", device="cuda", image_directory="."):
+        super().__init__(pretrained, device, image_directory)
         self.model = CLIPModel.from_pretrained(pretrained, cache_dir="models/clip")
+        self.simplify()
         self.model.to(device)
         self.processor = AutoProcessor.from_pretrained(pretrained, cache_dir="models/clip")
 
@@ -82,10 +93,13 @@ class TransformersCLIP(CLIP):
             outputs = self.model.get_image_features(output_hidden_states=True, **inputs)
             return outputs.flatten()
         
+    def simplify(self):
+        del self.model.text_model
+        
 class MultiCLIP(CLIP):
-    def __init__(self, pretrained:list, device, image_directory, use_cache):
-        super().__init__("_".join(pretrained), device, image_directory, use_cache)
-        self.models = [ CLIP.get_clip(p, "cpu", image_directory, use_cache=False) for p in pretrained ]
+    def __init__(self, pretrained:list, device, image_directory):
+        super().__init__("_".join(pretrained), device, image_directory)
+        self.models = [ CLIP.get_clip(p, "cpu", image_directory) for p in pretrained ]
         
     def get_image_features_tensor(self, image: Image) -> Tensor:
         ift = None
@@ -93,8 +107,25 @@ class MultiCLIP(CLIP):
             m.to(self.device)
             features = m.get_image_features_tensor(image)
             ift = features if ift is None else torch.cat([ift,features])
-            m.model.to('cpu')
+            #m.model.to('cpu')
         return ift
+    
+    def prepare_from_file(self, filepath, device="cuda"):
+        rel = os.path.relpath(filepath, self.image_directory)
+        if rel not in self.cached:
+            self.cached[rel] = self.get_image_features_tensor(Image.open(filepath))
+        return self.cached[rel].to(device)
+    
+    def precache(self, filepaths):
+        newfiles = { f for f in filepaths if os.path.relpath(f, self.image_directory) not in self.cached }
+        if not newfiles: return
+        for m in self.models:
+            m.precache(newfiles)
+            for f in tqdm(newfiles):
+                rel = os.path.relpath(f, self.image_directory)
+                features = m.prepare_from_file(f)
+                self.cached[rel] = features if rel not in self.cached is None else torch.cat([self.cached[rel],features])
+            m.model.to('cpu')
 
 if __name__=='__main__':
     c = CLIP.get_clip()
