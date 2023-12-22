9605a3d2f6d2875ae9e9be4787fd515e549ca57c
Chris <chris.goringe@gmail.com>
2023-12-22 16:30:12 +1100

    normalise

--- csv_to_score.py
+++ csv_to_score.py
@@ -1,4 +1,4 @@
-import csv, json, os
+import csv, json, os, statistics
 
 # the json file will be created in the same place
 csv_file = r"C:\Users\chris\Downloads\Peter_Mckinnon.csv"
@@ -18,6 +18,20 @@ def csv_to_json():
         for row in reader:
             if len(row)>max(filename_column,score_column) and row[filename_column] and row[score_column]:
                 scores[os.path.join(image_subfolder,row[filename_column])] = [float(row[score_column]),100]
+
+    all_score_values = list(scores[f][0] for f in scores)
+    all_score_mean = statistics.mean(all_score_values)
+    all_score_stdev = statistics.stdev(all_score_values)
+    print("Before: Scores mean {:>7.3f} std {:>7.3f}".format(all_score_mean, all_score_stdev))
+    
+    for f in scores:
+        scores[f][0] = (scores[f][0]-all_score_mean)/all_score_stdev
+
+    all_score_values = list(scores[f][0] for f in scores)
+    all_score_mean = statistics.mean(all_score_values)
+    all_score_stdev = statistics.stdev(all_score_values)
+    print(" After: Scores mean {:>7.3f} std {:>7.3f}".format(all_score_mean, all_score_stdev))
+
     scores['#meta#'] = { "source":"csv file" }
     with open(json_file,'w') as f:
         print(json.dumps(scores,indent=2), file=f)
