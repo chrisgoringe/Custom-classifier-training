d950831d3c20adc3a1d14e58ded0b3e1194ef58b
Chris <chris.goringe@gmail.com>
2024-02-25 12:38:56 +1100

    shrink CLIP models

--- model_shrink.py
+++ model_shrink.py
@@ -1,8 +1,27 @@
 import torch
-from transformers import CLIPModel, CLIPVisionConfig, CLIPVisionModelWithProjection, AutoModel, AutoProcessor, PretrainedConfig
+from transformers import CLIPModel, CLIPVisionConfig, CLIPVisionModelWithProjection, AutoModel, AutoProcessor, PretrainedConfig, CLIPProcessor
 import os, shutil, json
 from safetensors.torch import save_file, load_file
 from aim.torch.models import AIMForImageClassification
+
+VISION_CONFIG_DEFAULTS = {
+    "attention_dropout": 0.0,
+    "dropout": 0.0,
+    "hidden_act": "gelu",
+    "hidden_size": 1664,
+    "image_size": 224,
+    "initializer_factor": 1.0,
+    "initializer_range": 0.02,
+    "intermediate_size": 8192,
+    "layer_norm_eps": 1e-05,
+    "model_type": "clip_vision_model",
+    "num_attention_heads": 16,
+    "num_channels": 3,
+    "num_hidden_layers": 48,
+    "patch_size": 14,
+    "projection_dim": 1280,
+    "transformers_version": "4.24.0"
+  }
 
 def convert(pretrained, directory):
     if not os.path.exists(directory): os.makedirs(directory)
@@ -14,12 +33,24 @@ def convert(pretrained, directory):
     sd.pop('logit_scale',None)
     save_file(sd, f"{directory}/model.safetensors", {"format":"pt"})
 
-    vision_config = c.config.vision_config.get_config_dict()
-    with open(os.path.join(directory,"config.json"),'w') as f: print(json.dumps(vision_config, indent=2), file=f)
+    d = PretrainedConfig.get_config_dict(pretrained)
+    while isinstance(d,list) or isinstance(d,tuple): 
+        d = d[0]
+    d.pop("_commit_hash",None)
 
-    processor = AutoProcessor.from_pretrained(pretrained, cache_dir="models")
-    processor_config = processor.get_config_dict()
-    with open(os.path.join(directory,"preprocessor_config.json"),'w') as f: print(json.dumps(processor_config, indent=2), file=f)
+    config = {"architectures": ["CLIPVisionModelWithProjection"], "torch_dtype":"float16"}
+    for key in VISION_CONFIG_DEFAULTS: config[key] = d['vision_config'].get(key, VISION_CONFIG_DEFAULTS[key])
+
+    with open(os.path.join(directory,"config.json"),'w') as f: print(json.dumps(config, indent=2), file=f)
+
+    cached_directory = os.path.join("models", pretrained)
+    cached_directory = cached_directory.replace('/','--')
+    cached_directory = cached_directory.replace('\\','--')
+    cached_directory = os.path.join("models", cached_directory)
+    with open(os.path.join(cached_directory,'refs','main'), 'r') as f: 
+        main_snapshot_directory = os.path.join(cached_directory,"snapshots",f.readline())
+    for other_file in ["preprocessor_config.json",]:
+        shutil.copy(os.path.join(main_snapshot_directory,other_file), os.path.join(directory,other_file))
 
 def convert_aim(pretrained, directory):
     if not os.path.exists(directory): os.makedirs(directory)
@@ -39,14 +70,13 @@ def load(directory):
 def load_aim(directory):
     c = AIMForImageClassification.from_pretrained(directory)
 
-#convert("openai/clip-vit-large-patch14", "models/vit-vision-fp16")
-#load("models/vit-vision-fp16")
-for s in ["7B",]:
-    directory = os.path.join("models",f"aim-{s}-fp16")
-    convert_aim(f"apple/aim-{s}", directory)
+directory = "models/vit-large-p14-vision-fp16"
+model = "openai/clip-vit-large-patch14"
+is_aim = False
+
+if not is_aim:
+    convert(model, directory)
+    load(directory)
+else:
+    convert_aim(model, directory)
     load_aim(directory)
-
-
-pass
-
-
