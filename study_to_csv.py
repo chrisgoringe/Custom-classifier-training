43ee4e2751132be834fe45c0c4dabfcd2289f53d
Chris <chris.goringe@gmail.com>
2024-03-06 17:25:27 +1100

    fixes

--- study_to_csv.py
+++ study_to_csv.py
@@ -5,7 +5,7 @@ import optuna
 def parse_arguments():
     parser = argparse.ArgumentParser("Save study as csv")
     parser.add_argument('--database', default="sqlite:///db.sqlite")
-    parser.add_argument('--study_name', default=None)
+    parser.add_argument('--study_name', default=None, help="Default is the most recent study; --study_name=list to show available studies.")
     parser.add_argument('--outfile', default=None)
 
     Args, unknowns = parser.parse_known_args()
@@ -14,13 +14,16 @@ def parse_arguments():
 
 def main():
     Args = parse_arguments()
-    if Args.study_name and Args.outfile:
-        study:optuna.Study = optuna.study.load_study(study_name=Args.study_name, storage=Args.database)
-        df:DataFrame = study.trials_dataframe(attrs=('number', 'value', 'datetime_start', 'datetime_complete', 'duration', 'params', 'user_attrs'),)
-        df.to_csv(Args.outfile)
-    else:
-        print("--study_name and --outfile are both required. Available studies:")
+    study_name = Args.study_name or optuna.study.get_all_study_names(storage=Args.database)[-1]
+    if Args.study_name=='list':
         print("\n".join(optuna.study.get_all_study_names(storage=Args.database)))
+    else:
+        if Args.outfile:
+            study:optuna.Study = optuna.study.load_study(study_name=study_name, storage=Args.database)
+            df:DataFrame = study.trials_dataframe(attrs=('number', 'value', 'datetime_start', 'datetime_complete', 'duration', 'params', 'user_attrs'),)
+            df.to_csv(Args.outfile)
+        else:
+            print("--outfile required")
 
 if __name__=='__main__':
     main()
